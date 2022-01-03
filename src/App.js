00c7df53446e5c92f1c07942eb49c79c899f4268
Franklin Lainez <franklin.lainez2805@gmail.com>
2022-01-03 12:58:39 -0600

    finished project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchThunk,
+  selectStatus,
+  selectTodos,
+  setComplete,
+  setFilter,
+} from "./features/todos";
 
-function App() {
+const TodoItem = ({ todo }) => {
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <li
+      style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+      onClick={() => dispatch(setComplete(todo))}
+    >
+      {todo.title}
+    </li>
+  );
+};
+
+export default function App() {
+  const [value, setValue] = useState("");
+  const dispatch = useDispatch();
+  const todos = useSelector(selectTodos);
+  const status = useSelector(selectStatus);
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (!value.trim()) {
+      return;
+    }
+    const id = Math.random().toString(36);
+    const todo = { title: value, completed: false, id };
+    dispatch({ type: "todo/add", payload: todo });
+    setValue("");
+  };
+
+  if (status.loading === "pending") {
+    return <p>cargando...</p>;
+  }
+
+  if (status.loading === "rejected") {
+    return <p>{status.error}</p>;
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <input value={value} onChange={(e) => setValue(e.target.value)} />
+      </form>
+      <button onClick={() => dispatch(setFilter("all"))}>Mostrar todos</button>
+      <button onClick={() => dispatch(setFilter("complete"))}>
+        Completados
+      </button>
+      <button onClick={() => dispatch(setFilter("incomplete"))}>
+        Incompletos
+      </button>
+      <button onClick={() => dispatch(fetchThunk())}>Fetch</button>
+      <ul>
+        {todos.map((x) => (
+          <TodoItem key={x.id} todo={x} />
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
